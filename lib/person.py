b12653dba39dc737a81f12d4ab7c675778ac05a1
Walker2269-chichi <langatjeremiah22@gmail.com>
2025-01-12 17:21:34 +0300

    Initial commit

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
+    pass
     # Class body goes here
-
+    def talk(self):
+        print ("Hello World!")
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
     pass
+who = Person()
+who.talk()
+who.walk()
